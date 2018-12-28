b097cc9a18afcc3b1878c3ab7b179354938f5df4
xuetao8997 <46212040+xuetao8997@users.noreply.github.com>
2018-12-28 23:19:06 +0800

    Add files via upload

---
+++ hybrid-free-busy-certificate/1.ExportAuthCert.ps1
@@ -0,0 +1,13 @@
+﻿$thumbprint = (Get-AuthConfig).CurrentCertificateThumbprint
+ 
+if((test-path $env:SYSTEMDRIVE\OAuthConfig) -eq $false)
+{
+    md $env:SYSTEMDRIVE\OAuthConfig
+}
+cd $env:SYSTEMDRIVE\OAuthConfig
+ 
+$oAuthCert = (dir Cert:\LocalMachine\My) | where {$_.Thumbprint -match $thumbprint}
+$certType = [System.Security.Cryptography.X509Certificates.X509ContentType]::Cert
+$certBytes = $oAuthCert.Export($certType)
+$CertFile = "$env:SYSTEMDRIVE\OAuthConfig\OAuthCert.cer"
+[System.IO.File]::WriteAllBytes($CertFile, $certBytes)
---
+++ hybrid-free-busy-certificate/3.RegisterEndpoints.ps1
@@ -0,0 +1,10 @@
+﻿$externalAuthority="*.domain.com"
+ 
+$ServiceName = "00000002-0000-0ff1-ce00-000000000000";
+ 
+$p = Get-MsolServicePrincipal -ServicePrincipalName $ServiceName;
+ 
+$spn = [string]::Format("{0}/{1}", $ServiceName, $externalAuthority);
+$p.ServicePrincipalNames.Add($spn);
+ 
+Set-MsolServicePrincipal -ObjectID $p.ObjectId -ServicePrincipalNames $p.ServicePrincipalNames;
