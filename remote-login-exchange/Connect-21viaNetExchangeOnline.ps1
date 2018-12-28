d72b1a12015a738b92cb7afd3595d847a940bb6e
xuetao8997 <46212040+xuetao8997@users.noreply.github.com>
2018-12-28 23:21:06 +0800

    Add files via upload

---
+++ remote-login-exchange/Connect-21viaNetExchangeOnline.ps1
@@ -0,0 +1,3 @@
+﻿$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://partner.outlook.cn/PowerShell-LiveID/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
